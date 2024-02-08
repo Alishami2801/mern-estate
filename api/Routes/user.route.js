4522d4f1a3d16c672cc3967b5477bf5ac36f0d33
Mohamed Ali <145426317+Alishami2801@users.noreply.github.com>
2024-02-08 21:31:16 +0400

    create user update user api route

--- api/Routes/user.route.js
+++ api/Routes/user.route.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { test } from "../Controllers/user.controller.js";
+import { test, updateUser } from "../Controllers/user.controller.js";
+import { verifyToken } from "../utilis/verifyToken.js";
 
 const router = express.Router();
 
-router.get('/test', test )
+router.get('/test', test)
+router.post('/update/:id', verifyToken, updateUser )
+
 
 export default router;
