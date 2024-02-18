f29bc67eec26fd08481e39ba28a225ae5933ef59
Mohamed Ali <145426317+Alishami2801@users.noreply.github.com>
2024-02-18 20:36:29 +0400

    completed listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -17,7 +17,8 @@ import {
  signoutUserStart,signoutUserFailure, signoutUserSuccess
 } from '../redux/user/userSlice.js';
 import { useDispatch } from 'react-redux';
-// import { Link } from 'react-router-dom';
+ import { Link } from 'react-router-dom';
+ 
 export default function Profile() {
   const fileRef = useRef(null);
   const {currentUser, loading, error } = useSelector((state) => state.user);
@@ -219,12 +220,12 @@ export default function Profile() {
         >
           {loading ? 'Loading...' : 'Update'}
         </button>
-        {/* <Link
+         <Link
           className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'
           to={'/create-listing'}
         >
           Create Listing
-        </Link> */}
+        </Link> 
       </form>
       <div className='flex justify-between mt-5'>
         <span
